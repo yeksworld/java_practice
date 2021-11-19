7abc8345ec9e0cbcc716e5c47a407d1275f352bd
yunus <yunusemre4634@gmail.com>
2021-11-19 14:31:37 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 333; i++) {
+            int a = 3 * i;
+            sum += a;
+        }
+        System.out.println(sum);
+    }
+}
