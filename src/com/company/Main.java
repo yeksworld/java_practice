a38f3152f612a8202f0253274a7ef7544ef70fa2
yunus <yunusemre4634@gmail.com>
2021-11-22 01:37:03 +0200

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 333; i++) {
-            int a = 3 * i;
+        for (int i = 0; i <= 333; i++) {
+            int a = 3 * i + 1;
             sum += a;
         }
         System.out.println(sum);
+        System.out.println("----------------------------");
+        add();
+    }
+
+    public static void add() {
+        int sum = 0;
+        for (int i = 0; i <= 1000; i++) {
+            sum = i%3==1 ? sum+i:sum;
+
+          /*  if (i % 3 == 1) {
+                sum = sum + i;
+            }*/
+        }
+        System.out.println("sum = " + sum);
     }
 }
